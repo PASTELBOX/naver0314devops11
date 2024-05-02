1520a47ff7a6ca5c1d4d9847c54d61dd50d04d2a
PASTELBOX <147224562+PASTELBOX@users.noreply.github.com>
2024-05-02 15:20:11 +0900

    day0502

--- javawork/src/day0315/Ex10_Munje.java
+++ javawork/src/day0315/Ex10_Munje.java
@@ -18,39 +18,40 @@ public class Ex10_Munje {
 		 * 십원 : 9개
 		 * 일원 : 3개
 		 */
-		Scanner sc=new Scanner(System.in);
+		Scanner sc = new Scanner(System.in);
 		/*
-		int money,man,chun,baek,shib,ill;
-		
-		
-		System.out.println("금액은?");
-		money=sc.nextInt();
-		
-		man=money/10000;
-		money=money-(man*10000);
-		chun=money/1000;
-		money=money-(chun*1000);
-		baek=money/100;
-		money=money-(baek*100);
-		shib=money/10;
-		money=money-(shib*10);
-		ill=money;
-		
-		
-		System.out.println("만원:"+man+"장");
-		System.out.println("천원:"+chun+"장");
-		System.out.println("백원:"+baek+"개");
-		System.out.println("십원:"+shib+"개");
-		System.out.println("일원:"+ill+"개");
-		*/
+		 * int money,man,chun,baek,shib,ill;
+		 * 
+		 * 
+		 * System.out.println("금액은?");
+		 * money=sc.nextInt();
+		 * 
+		 * man=money/10000;
+		 * money=money-(man*10000);
+		 * chun=money/1000;
+		 * money=money-(chun*1000);
+		 * baek=money/100;
+		 * money=money-(baek*100);
+		 * shib=money/10;
+		 * money=money-(shib*10);
+		 * ill=money;
+		 * 
+		 * 
+		 * System.out.println("만원:"+man+"장");
+		 * System.out.println("천원:"+chun+"장");
+		 * System.out.println("백원:"+baek+"개");
+		 * System.out.println("십원:"+shib+"개");
+		 * System.out.println("일원:"+ill+"개");
+		 */
 		int money;
 		System.out.println("금액 입력");
-		money=sc.nextInt();
-		System.out.println("만원:"+(money/10000)+"장");
-		System.out.println("천원:"+(money%10000/1000)+"장");
-		System.out.println("백원:"+(money%1000/100)+"개");
-		System.out.println("십원:"+(money%100/10)+"개");
-		System.out.println("일원:"+(money%10)+"개");
+		money = sc.nextInt();
+		System.out.println("만원:" + (money / 10000) + "장");
+		System.out.println("천원:" + (money % 10000 / 1000) + "장");
+		System.out.println("백원:" + (money % 1000 / 100) + "개");
+		System.out.println("십원:" + (money % 100 / 10) + "개");
+		System.out.println("일원:" + (money % 10) + "개");
+
 	}
 
 }
--- javawork/src/day0315/Ex12_Operator.java
+++ javawork/src/day0315/Ex12_Operator.java
@@ -11,35 +11,34 @@ public class Ex12_Operator {
 		 * 조건식 ? 참 : 거짓
 		 * 두개의 숫자를 입력 후 더 큰 숫자를 출력하시ㅗ
 		 */
-		Scanner sc=new Scanner(System.in);
-//		int su1,su2,max;
-//		System.out.println("두개의 숫자 입력");
-//		su1=sc.nextInt();
-//		su2=sc.nextInt();
-//		
-//		max=su1>su2?su1:su2;
-//		System.out.println("max="+max);
-		
+		Scanner sc = new Scanner(System.in);
+		// int su1,su2,max;
+		// System.out.println("두개의 숫자 입력");
+		// su1=sc.nextInt();
+		// su2=sc.nextInt();
+		//
+		// max=su1>su2?su1:su2;
+		// System.out.println("max="+max);
+
 		/*
 		 * 조건(삼항) 연산자
 		 * 조건식 ? 참 : 거짓
 		 * 세개의 숫자를 입력 후 더 큰 숫자를 출력하시ㅗ
 		 */
-		
-		int su1,su2,su3,max;
+
+		int su1, su2, su3, max;
 		System.out.println("세개의 숫자 입력");
-		su1=sc.nextInt();
-		su2=sc.nextInt();
-		su3=sc.nextInt();
-		
-		//max=su1>su2&&su1>su3?su1:
-		//	su2>su1&&su2>su3?su2:
-		//		su3;
-		
-		max=su1>su2&&su1>su3?su1:
-			su2>su3?su2:su3;
-		
-		System.out.println("max="+max);
+		su1 = sc.nextInt();
+		su2 = sc.nextInt();
+		su3 = sc.nextInt();
+
+		// max=su1>su2&&su1>su3?su1:
+		// su2>su1&&su2>su3?su2:
+		// su3;
+
+		max = su1 > su2 && su1 > su3 ? su1 : su2 > su3 ? su2 : su3;
+
+		System.out.println("max=" + max);
 
 	}
 
