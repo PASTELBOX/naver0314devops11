58fb135bbba47171454deb4c01a78aeb9595f1f7
PASTELBOX <147224562+PASTELBOX@users.noreply.github.com>
2024-05-13 17:37:09 +0900

    day0513

--- javawork/StartWebProject/src/main/java/data/servlet/HelloServlet.java
+++ javawork/StartWebProject/src/main/java/data/servlet/HelloServlet.java
@@ -1,31 +1,35 @@
-package test.servlet;
+package data.servlet;
 
 import java.io.IOException;
-import java.io.PrintWriter;
+import java.util.Date;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.jsp.JspWriter;
 
 /**
  * Servlet implementation class HelloServlet
  */
-@WebServlet("/hello.do")
+@WebServlet("/study1")
 public class HelloServlet extends HttpServlet {
 	
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		//response.getWriter().append("Served at: ").append(request.getContextPath());
-		PrintWriter out=response.getWriter();
-		out.append("<h2>Hello Servlet!!</h2>");
+		//request 에 값 저장
+		request.setAttribute("message", "안녕하세요. 비트캠프입니다");
+		request.setAttribute("today", new Date());
+		request.setAttribute("num1", 8);
+		request.setAttribute("num2", 5);
+		
+		//test1.jsp 로 포워드(url안바뀜,response,request 그래도 전달)
+		RequestDispatcher rd=request.getRequestDispatcher("./day0513/test1.jsp");
+		rd.forward(request, response);		
 	}
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
+	
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		doGet(request, response);
