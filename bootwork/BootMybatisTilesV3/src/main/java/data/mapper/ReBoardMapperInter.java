e4b847b7daf67286028997fee9360f5268e86250
PASTELBOX <147224562+PASTELBOX@users.noreply.github.com>
2024-05-27 15:10:35 +0900

    day0527

--- bootwork/BootMybatisTilesV3/src/main/java/data/mapper/ReBoardMapperInter.java
+++ bootwork/BootMybatisTilesV3/src/main/java/data/mapper/ReBoardMapperInter.java
@@ -1,8 +1,13 @@
 package data.mapper;
 
+import data.dto.ReBoardDto;
 import org.apache.ibatis.annotations.Mapper;
+
+import java.util.Map;
 
 @Mapper
 public interface ReBoardMapperInter {
     public int getMaxNum();
+    public void updateRestep(Map<String, Integer> map);//regroup, restep
+    public void insertBoard(ReBoardDto dto);
 }
--- bootwork/BootMybatisTilesV3/src/main/java/data/service/ReBoardService.java
+++ bootwork/BootMybatisTilesV3/src/main/java/data/service/ReBoardService.java
@@ -1,13 +1,36 @@
 package data.service;
 
+import data.dto.ReBoardDto;
 import data.mapper.ReBoardMapperInter;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Service
 @RequiredArgsConstructor
 public class ReBoardService {
     @NonNull
     private ReBoardMapperInter boardMapper;
+
+    public int getMaxNum()
+    {
+        return boardMapper.getMaxNum();
+    }
+
+    public void updateRestep(int regroup,int restep)
+    {
+        Map<String, Integer> map=new HashMap<>();
+        map.put("regroup",null);
+        map.put("restep",null);
+
+        boardMapper.updateRestep(map);
+    }
+
+    public void insertBoard(ReBoardDto dto)
+    {
+
+    }
 }
